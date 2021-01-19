5dd2264531ec9dcb4bedf0547b8f4647daec454a
Amirparsa Sal <amirparsa.s@aut.ac.ir>
2021-01-20 02:41:45 +0330

    Add CircularLinkedList class

--- Data Structures/LinkedList/DriverClass.java
+++ Data Structures/LinkedList/DriverClass.java
@@ -18,7 +18,24 @@ public class DriverClass{
 		// }
 		// System.out.println(list);
 
-		DoublyLinkedList<Integer> list = new DoublyLinkedList<>();
+		// DoublyLinkedList<Integer> list = new DoublyLinkedList<>();
+		// list.addLast(1);
+		// System.out.println(list);
+		// list.addLast(2);
+		// System.out.println(list);
+		// list.addLast(3);
+		// System.out.println(list);
+		// Iterator<Integer> it = list.iterator();
+		// while(it.hasNext()){
+		// 	Integer i = it.next();
+		// 	if (i<=2)
+		// 		it.remove();
+		// }
+		// System.out.println(list);
+		// list.removeLast();
+		// System.out.println(list);
+		
+		CircularLinkedList<Integer> list = new CircularLinkedList<>();
 		list.addLast(1);
 		System.out.println(list);
 		list.addLast(2);
@@ -34,6 +51,5 @@ public class DriverClass{
 		System.out.println(list);
 		list.removeLast();
 		System.out.println(list);
-		
 	}
 }
