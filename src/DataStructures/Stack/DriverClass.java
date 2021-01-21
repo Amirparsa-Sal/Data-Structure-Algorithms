b83393eef9a02409c54c6d919e6d7f3521947794
Amirparsa Sal <amirparsa.s@aut.ac.ir>
2021-01-22 02:56:02 +0330

    Add Stack class

---
+++ src/DataStructures/Stack/DriverClass.java
@@ -0,0 +1,15 @@
+package DataStructures.Stack;
+
+public class DriverClass{
+
+	public static void main(String[] args) {
+		StackWithLinkedList<Integer> list = new StackWithLinkedList<>();
+		System.out.println(list);
+		list.push(1);
+		System.out.println(list);
+		list.push(2);
+		System.out.println(list);
+		list.pop();
+		System.out.println(list);
+	}
+}
---
+++ src/DataStructures/Stack/StackWithLinkedList.java
@@ -0,0 +1,55 @@
+package DataStructures.Stack;
+
+import java.util.LinkedList;
+import java.util.Iterator;
+import java.lang.StringBuilder;
+
+public class StackWithLinkedList<T> implements Iterable<T>{
+
+	private LinkedList<T> list;
+
+	public StackWithLinkedList(){
+		list = new LinkedList<>();
+	}
+
+	public int size(){
+		return list.size();
+	}
+
+	public boolean isEmpty(){
+		return size() == 0;
+	}
+
+	public void push(T key){
+		list.addFirst(key);
+	}
+
+	public T pop(){
+		return list.removeFirst();
+	}
+
+	public T peek(){
+		return list.peekFirst();
+	}
+
+	@Override public String toString(){
+		if (size() == 0)
+			return "[]";
+		if (size() == 1)
+			return "Stack(1)[" + peek().toString() + "]";
+		StringBuilder sb = new StringBuilder("Stack(" + size() + ")[");
+		Iterator<T> it = list.iterator();
+		while(it.hasNext()){
+			T key = it.next();
+			sb.append(key);
+			if (it.hasNext())
+				sb.append(" ->  ");
+		}
+		sb.append("]");
+		return sb.toString();
+	}
+
+	@Override public Iterator<T> iterator(){
+		return list.iterator();
+	}
+}
